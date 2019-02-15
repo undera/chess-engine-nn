c84e269f03b817305c4be729600f75fb959ab260
Andrey Pohilko <apc4@ya.ru>
2019-02-15 19:33:41 +0300

    Filter positions

--- objects.py
+++ objects.py
@@ -1,9 +1,12 @@
+# Inspired by TCEC Season 14 - Superfinal
+# where Leela was trying to fry Stockfish
+
 import logging
 
 import keras
 import numpy as np
 from keras import Input
-from keras.layers import Dense, Activation
+from keras.layers import Dense
 from keras.utils import plot_model
 
 STARTING_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
@@ -18,15 +21,21 @@ class Board(object):
         self.move_num = 0
         self.active_index = 0
         self.castling_flags = "KQkq"
+        self.moves = []
+        self.starting_fen = None
 
-    def playable(self):
+    def is_playable(self):
         return True
 
     def make_move(self, move):
-        # log the move
-        pass
+        # check who's move it is
+        # tick active_index and move_num
+        logging.info("New Move")
+        self.moves.append(move)
+        # check 3-fold and 50-move
 
     def from_fen(self, fen):
+        self.starting_fen = fen
         placement, active_colour, self.castling_flags, enpassant, halfmove, fullmove = fen.split(' ')
         self._50move_counter = int(halfmove)
         self.move_num = fullmove
@@ -58,8 +67,8 @@ class NN(object):
         positions = Input(shape=(8 * 8 * len(PIECE_MAP),))
         hidden = Dense(64, activation="sigmoid")(positions)
         hidden = Dense(64, activation="sigmoid")(hidden)
-        out_from = Dense(64, activation="sigmoid")(hidden)
-        out_to = Dense(64, activation="sigmoid")(hidden)
+        out_from = Dense(64, activation="tanh")(hidden)
+        out_to = Dense(64, activation="tanh")(hidden)
 
         model = keras.Model(inputs=[positions], outputs=[out_from, out_to])
         model.compile(optimizer='nadam',
@@ -71,7 +80,12 @@ class NN(object):
     def query(self, brd):
         data = brd.piece_placement.flatten()[np.newaxis, ...]
         res = self._model.predict_on_batch(data)
-        return res[0][0], res[1][0]
+
+        frm1 = res[0][0]
+        frm2 = np.reshape(frm1, (-1, 8))
+        tto1 = res[1][0]
+        tto2 = np.reshape(tto1, (-1, 8))
+        return frm2, tto2
 
 
 class Player(object):
@@ -85,26 +99,45 @@ class Player(object):
         self.nn = NN()
 
     def get_move(self):
-        weights_from, weigts_to = self.nn.query(self.board)
+        weights_from, weights_to = self.nn.query(self.board)
+        self._filter_positions(weights_from, 1 if self.piece_index else 0)
+        self._filter_positions(weights_to, 0 if self.piece_index else 1)
 
-        move = self._choose_move(weights_from, weigts_to)
+        move = self._choose_move(weights_from, weights_to)
         return move
 
     def _choose_move(self, weights_from, weigts_to):
         # check 3-fold here
         return None
 
+    def _filter_positions(self, weights, index):
+        for rank in range(8):
+            for col in range(8):
+                cell = self.board.piece_placement[rank][col]
+                idx = np.flatnonzero(cell)
+                if not idx.size and index:
+                    continue
+
+                if not idx.size or idx[0] % 2 != index:
+                    weights[rank][col] = None
+
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
     board = Board()
     board.from_fen(STARTING_POSITION)
 
     white = Player(board, 0)
     black = Player(board, 1)
 
-    while board.playable():
+    while True:
         wmove = white.get_move()
         board.make_move(wmove)
+        if not board.is_playable():
+            break
 
         bmove = black.get_move()
         board.make_move(bmove)
+        if not board.is_playable():
+            break

1aed9ade5dfd71e1d5f2a9f3eaf0ddff9a0db4b3
Andrey Pokhilko <apc4@ya.ru>
2019-07-27 23:17:40 +0300

    Play with stockfish

---
+++ uci.py
@@ -0,0 +1,32 @@
+import logging
+import random
+
+import chess
+from chess import WHITE
+from chess.engine import SimpleEngine
+
+from chessnn import BoardOptim, is_debug
+from chessnn.nn import NNChess
+from chessnn.player import Player
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG if is_debug() else logging.INFO)
+
+    engine = SimpleEngine.popen_uci("stockfish")
+
+    try:
+        board = BoardOptim.from_chess960_pos(random.randint(0, 959))
+        nn = NNChess("nn.hdf5")
+        white = Player(WHITE, nn)
+        white.board = board
+
+        while not board.is_game_over():
+            if not white.makes_move(0):
+                break
+
+            result = engine.play(board, chess.engine.Limit(time=0.100))
+            board.push(result.move)
+
+        logging.info("Result: %s", board.result())
+    finally:
+        engine.quit()
